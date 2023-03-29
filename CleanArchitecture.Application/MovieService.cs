e012f02f2b1a3f18bbc1a0df8e76dee1a95d6a14
deb240485 <deb240485rana@gmail.com>
2023-03-29 12:05:53 +0530

    Implemented API with cleanarchitecture.

---
+++ CleanArchitecture.Application/MovieService.cs
@@ -0,0 +1,23 @@
+﻿using CleanArchitecture.Application.Contracts;
+using CleanArchitecture.Domain;
+
+namespace CleanArchitecture.Application
+{
+    public class MovieService : IMovieService
+    {
+        private readonly IMovieRepository _movieRepository;
+        public MovieService(IMovieRepository movieRepository)
+        {
+            _movieRepository = movieRepository;
+        }
+        public async Task<bool> CreateMovie(Movie movie)
+        {
+            return await _movieRepository.CreateMovie(movie);
+        }
+
+        public async Task<List<Movie>> GetAllMovies()
+        {
+            return await _movieRepository.GetAllMovies();
+        }
+    }
+}
